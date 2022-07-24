0e3a15ab12160eaea942c25d0e7f7a1ae88a3044
mitch-j-H <mitchell.henwood2022@campus-eni.fr>
2022-07-24 18:53:57 +0200

    added OneToMany City>Location && getters/setters

--- src/Entity/City.php
+++ src/Entity/City.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CityRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CityRepository::class)]
@@ -18,6 +20,10 @@ class City
 
     #[ORM\Column(length: 5)]
     private ?string $postcode = null;
+
+    #[ORM\OneToMany( mappedBy: "city", targetEntity: Location::class)]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Collection $locations = null;
 
     public function getId(): ?int
     {
@@ -46,5 +52,31 @@ class City
         $this->postcode = $postcode;
 
         return $this;
+    }
+
+    /**
+     * @return Collection<int, Location>
+     */
+    public function getLocations():Collection {
+        return $this->locations;
+    }
+
+    public function addLocations(Location$location): self {
+        if(!$this->locations->contains($location)) {
+            $this->locations[] = $location;
+            $location->setCity($this);
+        }
+        return $this;
+    }
+
+    public function removeStudents(Location$location) {
+        if($this->locations->contains($location)) {
+            $this->locations->removeElement($location);
+
+            if($location->getCity() === $this) {
+                $location->setCity(null);
+            }
+            return $this;
+        }
     }
 }
